b9330d3632b28d02d6f2148dd9927d7e192d543c
Paulo Martinez <paulomartinez1999@gmail.com>
2023-02-14 21:37:00 -0500

    camara en recuadro de interfaz

--- interfaz.py
+++ interfaz.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import subprocess
+
 import torch as torch
 from PIL import Image, ImageTk
 import cv2
@@ -8,40 +8,56 @@ import imutils
 
 #Interfaz
 ventana = tk.Tk()
-ventana.geometry("1241x600+200+10")
+ventana.geometry("994x699+200+10")
 ventana.title("Interfaz")
 ventana.resizable(width=False, height=False)
-fondo = tk.PhotoImage(file="interfaz.png")
+fondo = tk.PhotoImage(file="camara.png")
 fondo1 = tk.Label(ventana, image=fondo).place(x=0, y=0, relwidth=1, relheight=1)
 
-# Crear la entrada de texto
-entry = tk.Entry(ventana, cursor="hand2", width=38)
-entry.config(font=('Helvetica', 15))
-entry.place(x=664, y=210)
+video = None
 
+def video_stream():
+    global video
+    video = cv2.VideoCapture(0)
+    iniciar()
 
-def run_command(command):
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    return stdout, stderr
+def iniciar():
+    global video
+    ret, frame = video.read()
 
+    if ret == True:
+        frame = imutils.resize(frame, width=491, height=900)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(frame)
+        image = ImageTk.PhotoImage(image=img)
+        etiq_video.configure(image=image)
+        etiq_video.image = image
+        etiq_video.after(9, iniciar)
+    else:
+        etiq_video.image = ""
+        video.release()
+
+def quitar():
+    global video
+    #etiq_video.place_forget()
+    video.release()
 
 #Color
 fondo_boton = "#FFF"
 
 
 #BOTONES
-# Crear una función lambda que llame a la función con los parámetros
-my_lambda = lambda: run_command("python detect.py")
+boton = tk.Button(ventana, text="INICIAR", bg=fondo_boton, relief="flat",
+                  cursor="hand2", command=video_stream, width=15, height=2, font=("Calisto MT", 12, "bold"))
+boton.place(x=165, y=590)
 
-boton = tk.Button(ventana, text="INGRESAR", bg=fondo_boton, relief="flat",
-                  cursor="hand2", command=my_lambda, width=35, height=2, font=("Calisto MT", 15, "bold"))
-boton.place(x=664, y=344)
+boton2 = tk.Button(ventana, text="APAGAR", bg=fondo_boton, relief="flat",
+                  cursor="hand2", command=quitar, width=15, height=2, font=("Calisto MT", 12, "bold"))
+boton2.place(x=665, y=590)
 
-boton1 = tk.Button(ventana, text="REPORTES", bg=fondo_boton, relief="flat",
-                  cursor="hand2", command=my_lambda, width=35, height=2, font=("Calisto MT", 15, "bold"))
-boton1.place(x=664, y=503)
-
+#Etiqueta
+etiq_video = tk.Label(ventana, bg="black")
+etiq_video.place(x=247, y=119)
 
 
 ventana.mainloop()
